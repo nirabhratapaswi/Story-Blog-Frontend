09ab7ccb2f5560ea5c8a375d3d3cfcf55775e207
Nirabhra Tapaswi <nirabhratapaswi@gmail.com>
2018-12-04 20:03:43 +0530

    Changed backend host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: "http://localhost",/*"http://ec2-35-154-80-242.ap-south-1.compute.amazonaws.com",*/
+  baseUrl: "http://ec2-52-66-208-89.ap-south-1.compute.amazonaws.com",/*"http://ec2-35-154-80-242.ap-south-1.compute.amazonaws.com",*/
   port: 3000
 };
 
